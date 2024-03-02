df17b46e3fd5241a34f3e9332be454d0c95506f6
Safeya-Yasien <bluesky7yas@gmail.com>
2024-03-02 16:19:04 +0200

    #products

---
+++ js/main.js
@@ -0,0 +1,44 @@
+fetch("https://fakestoreapi.com/products/category/electronics")
+  .then((res) => res.json())
+  .then((json) => displayData(json));
+
+function displayData(data) {
+  console.log(data);
+
+  const productsContent = document.querySelector(".products-content");
+
+  let html = ""; // Initialize an empty string to store HTML content
+
+  data.forEach((product) => {
+    html += `
+      <div class="col-lg-4 col-md-4 col-sm-6 mb-4">
+        <div class="box">
+          <div class="product-header">
+            <h2>${product.title}</h2>
+            <p>${product.description}</p>
+          </div>
+
+          <img src="${product.image}" />
+
+          <div class="product-footer">
+            <div class="reviews">
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+            </div>
+
+            <div class="product-price">
+              <p>${product.price}</p>
+              <i class="fa-solid fa-cart-shopping"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  productsContent.innerHTML = `<div class="row">${html}</div>`; // Set the HTML content inside the productsContent element
+}
