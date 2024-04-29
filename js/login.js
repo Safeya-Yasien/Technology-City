aefdff2b06ee5070e5206ca211d7334677310ee9
Safeya-Yasien <bluesky7yas@gmail.com>
2024-04-29 13:11:46 +0300

    signIn & singUp

---
+++ js/login.js
@@ -0,0 +1,45 @@
+const loginForm = document.querySelector(".login form");
+
+const api = "http://127.0.0.1:5000/api/authenticate";
+
+loginForm.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const email = loginForm.querySelector("#email").value;
+  const password = loginForm.querySelector("#password").value;
+
+  // Validation
+  if (email.trim() === "") {
+    console.log("Please enter your email.");
+    return;
+  }
+
+  if (password.trim() === "") {
+    console.log("Please enter your password.");
+    return;
+  }
+
+  const responseBody = {
+    emial: email,
+    password: password,
+  };
+
+  try {
+    const response = await fetch(api, {
+      method: "POST",
+      headers: "application/json",
+      body: JSON.stringify(responseBody),
+    });
+
+    const responseData = await response.json();
+
+    if (response.ok) {
+      console.log("Login successful!");
+      loginForm.reset();
+    } else {
+      console.error("Login failed:", responseData.message);
+    }
+  } catch (error) {
+    console.error("Login Faild");
+  }
+});
---
+++ js/register.js
@@ -0,0 +1,65 @@
+const api = "http://127.0.0.1:5000/api/signup";
+
+const signUpForm = document.querySelector(".register form");
+
+signUpForm.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const name = signUpForm.querySelector("#name").value;
+  const email = signUpForm.querySelector("#email").value;
+  const password = signUpForm.querySelector("#password").value;
+  const repeatPassword = signUpForm.querySelector("#repeat-password").value;
+
+  // Validation
+  if (name.trim() === "") {
+    console.log("Please enter your name.");
+    return;
+  }
+
+  if (email.trim() === "") {
+    console.log("Please enter your email.");
+    return;
+  }
+
+  if (password.trim() === "") {
+    console.log("Please enter a password.");
+    return;
+  }
+
+  if (password !== repeatPassword) {
+    console.log("Passwords do not match.");
+    return;
+  }
+
+  if (!document.getElementById("agree-term").checked) {
+    console.log("Please agree to the terms of service.");
+    return;
+  }
+
+  const requestBody = {
+    name: name,
+    emial: email,
+    password: password,
+  };
+
+  try {
+    const response = await fetch(api, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    const responseData = await response.json();
+
+    if (response.ok) {
+      console.log("registration successful");
+      signUpForm.reset();
+    } else {
+      console.error("registration failed:", responseData.message);
+    }
+  } catch (error) {
+    console.error("registration failed:", responseData.message);
+  }
+});
